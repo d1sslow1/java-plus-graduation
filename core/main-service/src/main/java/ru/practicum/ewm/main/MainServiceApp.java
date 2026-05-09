c0abfb0950ebc1504ca6cd903c057db1e6617937
d1sslow <artemdanielyan2006@yandex.ru>
2026-05-10 03:01:55 +0700

    initial commit

---
+++ core/main-service/src/main/java/ru/practicum/ewm/main/MainServiceApp.java
@@ -0,0 +1,15 @@
+package ru.practicum.ewm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum"})
+@EnableDiscoveryClient
+public class MainServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceApp.class, args);
+    }
+}
